68cca4d2d1cdb1a46e0923985389b10507b20f73
Cristian Díaz <88348311+cristiandiazperdomo@users.noreply.github.com>
2022-02-19 13:10:47 -0300

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -9,70 +9,33 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
-/*
-const CATEGORIES_URL            = "http://localhost:3000/category";
-const PUBLISH_PRODUCT_URL       = "http://localhost:3000/publish";
-const CATEGORY_INFO_URL         = "http://localhost:3000/categoryinfo";
-const PRODUCTS_URL              = "http://localhost:3000/products";
-const PRODUCT_INFO_URL          = "http://localhost:3000/productinfo";
-const PRODUCT_INFO_COMMENTS_URL = "http://localhost:3000/productcomments";
-const CART_INFO_URL             = "http://localhost:3000/cartinfo";
-const CART_BUY_URL              = "http://localhost:3000/cartbuy";
-*/
+const form = document.querySelector('.form')
+const results = document.querySelector('.resultado')
 
+const filterProducts = async () => {
 
-const preview = (event) => { //MOSTRAR IMAGEN DEL USUARIO
+    results.innerHTML = '';
 
-    var input = event.target;
+    const texto = form.value.toLowerCase();
 
-    const reader = new FileReader();
-
-    reader.addEventListener('load', () => {
-        let dataUrl = reader.result
-
-
-        let la_data = "";
-        la_data += `<img width="200px" height="100px" class="border border-secondary"  src="` + dataUrl + `" >`
-        document.querySelector('.lapic').innerHTML = la_data;
-
-    })
-
-    reader.readAsDataURL(input.files[0]);
-}
-//------------------------
-//BOTONES y TABLA   //BUSQUEDA DE PRODUCTOS
-
-const botonBuscar = document.querySelector('.btnes')
-const formulario = document.querySelector('.form')
-const resultado = document.querySelector('.resultado')
-const btnDelete = document.querySelector('.btnDelete')
-
-//-----------------------
-
-const filtrar = async () => {
-
-    resultado.innerHTML = '';
-
-    const texto = formulario.value.toLowerCase();
-
-    let currentCategories = await getJSONDataUnCharge(PRODUCTS_URL)
+    let currentCategories = await getJSONDataUnCharge(PRODUCTS_URL);
 
     if (currentCategories.status === "ok") {
 
-        let currentCategoriesArrays = currentCategories.data
+        let currentCategoriesArrays = currentCategories.data;
 
         for (let producto of currentCategoriesArrays) {
 
             let name = producto.name.toLowerCase();
 
             if (name.indexOf(texto) !== -1) {
-                resultado.innerHTML += `
+                results.innerHTML += `
                 <td class="d-md-block m-2"> ${producto.name} - ${producto.cost}  ${producto.currency} ${producto.description}</td>`
             }
 
         }
-        if (resultado.innerHTML === '') {
-            resultado.innerHTML += `
+        if (results.innerHTML === '') {
+            results.innerHTML += `
                 <td class="m-2">Pruducto no encontrado...</td>
                 `
         }
@@ -80,59 +43,21 @@ const filtrar = async () => {
     }
 
 }
-const borrar = () => //borrar texto de busqueda
-    {
-        resultado.innerHTML = "";
-    }
 
-//--------
+const cleanSearchBar = () => results.innerHTML = "";
 
-//LOGEADO
+let isLogged = JSON.parse(localStorage.getItem('DATA'));
 
+const userCanNavegate = () => isLogged ? console.log("INGRESO CORRECTO") : window.location.replace("login.html");
 
-let bLogin = JSON.parse(localStorage.getItem('DATA'));
-let dataOnGoogle = JSON.parse(localStorage.getItem('userDataGoogle'));
+const showNameinTopNavigationBar = () => {
 
-if (bLogin || dataOnGoogle) {
-    console.log("INGRESO CORRECTO")
-} else {
-    window.location.replace("login.html");
-}
-//-------
+    let htmlContentToAppendUser = document.querySelector(".userName")
 
-//NOMBRE DE USUARIO
-
-
-
-const showNameinLocalStorage = () => {
-    if (bLogin) {
-        let showUserName = JSON.parse(localStorage.getItem('DATA'));
-
-        let htmlContentToAppendUser = "";
-        htmlContentToAppendUser += `
-        
-            <a class="py-2 d-none d-md-inline-block" href="#"> ` + showUserName[0].name + `</a>
-        
-        `
-
-        document.querySelector(".userSpace").innerHTML = htmlContentToAppendUser;
-
-
-    } else {
-
-        let htmlContentToAppendUser = "";
-        htmlContentToAppendUser += `
-        
-            <a class="py-2 d-none d-md-inline-block" href="#"> ` + dataOnGoogle.jf.toLowerCase() + `</a>
-        
-        `
-
-        document.querySelector(".userSpace").innerHTML = htmlContentToAppendUser;
-    }
+    htmlContentToAppendUser.innerHTML += ` <a class="py-2 d-none d-md-inline-block" href="#"> ` + isLogged[0].name + `</a>`
 
 }
-showNameinLocalStorage()
-//-------
+
 var showSpinner = function() {
     document.getElementById("spinner-wrapper").style.display = "block";
 }
@@ -166,31 +91,12 @@ var getJSONData = function(url) {
         });
 }
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-
-//-------
-//DELETE INFO
-const DEL_DATA = document.getElementById('deleteInfo')
-
-function cerrarSesion() {
-
-    if (bLogin) {
-        localStorage.removeItem('DATA');
-        window.location.replace("index.html");
-    } else {
-        localStorage.removeItem('userDataGoogle')
-        window.location.replace("index.html");
-    }
+function logOut() {
+    localStorage.removeItem('DATA');
+    window.location.replace("index.html");
 }
-//-------
 
-//EVENTS BUTTONS
-DEL_DATA.addEventListener('click', cerrarSesion)
-
-
-let getJSONDataUnCharge = function(url) { //GETSONDATA sin el spinner ya que me resultaba molesto y se ejecutaba cada vez que apretas una letra.
+let getJSONDataUnCharge = function(url) { //SPINNERLESS TO EVENT KEYUP
     var result = {};
 
     return fetch(url)
@@ -213,20 +119,19 @@ let getJSONDataUnCharge = function(url) { //GETSONDATA sin el spinner ya que me 
 
             return result;
         });
+
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    //USER IS LOGGED
+    userCanNavegate()
+    //SHOW NAME IN TOP NAVIGATION BAR
+    showNameinTopNavigationBar()
 
+    form.addEventListener('keyup', filterProducts);
 
-//----------
-//eventos de BUSCAR
+    document.querySelector('.btnes').addEventListener('click', filterProducts);
+    document.querySelector('.btnDelete').addEventListener('click', cleanSearchBar);
 
-
-botonBuscar.addEventListener('click', filtrar)
-
-formulario.addEventListener('keyup', filtrar)
-
-//evento BORRAR LISTA
-
-btnDelete.addEventListener('click', borrar)
-
-//------------
+    document.getElementById('deleteInfo').addEventListener('click', logOut) //DELETE USERINFO
+})
