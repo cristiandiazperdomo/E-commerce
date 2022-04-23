36c0afe1fe1b4d7b8b66083c23023ee7f56f8961
cristiandiazperdomo <danubiodiaz2003edf@gmail.com>
2022-04-23 00:15:20 -0300

    product-info.js refactoring

--- js/product-info.js
+++ js/product-info.js
@@ -21,7 +21,7 @@ function showProduct(currentProductArray) {
 	                        <small class="text-muted">` + product.soldCount + ` artículos </small>
 	                    </div>
 	                    <div>
-	                        <small class="text-muted average-stars">${averageStars()}</small>
+	                        <small class="text-muted average-stars">${averageStarsOfComments()}</small>
 	                    </div>
 	                </div>
                     <div class="d-flex w-100 justify-content-between">
@@ -31,24 +31,29 @@ function showProduct(currentProductArray) {
             </div>
         </div>`
 
-    carouselZone.innerHTML += `
-    <div class="list-group-item list-group-item-action">
-	    <h4 class="my-4">` + "Precio: " + ` ` + "U$D" + ` ` + product.cost + `</h4>
-		<p class="m-0 mb-2 product-descrition">Descripción</p>
-		<p class="">` + product.description + `</p>
-	</div>`
+	carouselZone.innerHTML += `
+	    <div class="list-group-item list-group-item-action">
+		    <h4 class="my-4">` + "Precio: " + ` ` + "U$D" + ` ` + product.cost + `</h4>
+			<p class="m-0 mb-2 product-descrition">Descripción</p>
+			<p class="">` + product.description + `</p>
+		</div>
+	`
 
-	dataProd += `<div class="carousel-item active">
-                	<img  src="` + product.images[0] + `" class="d-block w-100">
-                </div>`
+	dataProd += `
+		<div class="carousel-item active">
+           	<img  src="` + product.images[0] + `" class="d-block w-100">
+        </div>
+    `
 
 	for (let i = 1; i < product.images.length; i++) {
 
-		dataProd += `<div class="carousel-item">
-	                	<img src="` + product.images[i] + `" class="d-block w-100">
-	                </div>`
+		dataProd += `
+			<div class="carousel-item">
+	        	<img src="` + product.images[i] + `" class="d-block w-100">
+	        </div>
+	    `
 	}
-	
+
 	document.getElementById("dataImage").innerHTML = dataProd;
 
 }
@@ -129,7 +134,7 @@ const canDeleteComments = (user, numberOfComments) => {
 					</svg>
 					<p>Eliminar</p>	
 				</li>
-				<li class="options salchicha dropdown-item">
+				<li class="options editor-option dropdown-item">
 					<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 172 172" style=" fill:#000000;">
 						<g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path>
 						<g fill="#cccccc"><path d="M111.27416,21.06122c-5.96045,-0.05251 -11.67736,2.36179 -15.79592,6.67076l-57.21564,57.39047c-0.50579,0.4059 -0.87441,0.95764 -1.0558,1.58028l-7.37006,33.87141c-0.20171,1.21604 0.1893,2.4553 1.05237,3.33538c0.64837,0.66129 1.5318,1.03955 2.45783,1.05238h0.70273l33.87141,-7.37006c0.65981,-0.10987 1.27132,-0.41563 1.7551,-0.87755l57.39389,-57.39047c4.192,-4.18351 6.53078,-9.87366 6.49251,-15.79592c0.00865,-5.96899 -2.32215,-11.70369 -6.49251,-15.97417c-4.18351,-4.192 -9.87366,-6.53078 -15.79592,-6.49251zM111.27416,28.08163c4.08029,0.00009 7.99081,1.63325 10.85922,4.53514c2.86841,2.9019 4.45606,6.83111 4.4088,10.91113c0.05722,4.01728 -1.52756,7.8839 -4.38776,10.70544l-21.5857,-21.76395c2.84403,-2.82647 6.69581,-4.40517 10.70544,-4.38776zM95.65306,37.55987l21.58913,21.5857l-50.02041,49.84558l-21.58913,-21.41087zM42.82518,94.60069l17.37619,17.37619l-22.29185,4.91223zM27.20408,143.91837c-1.93863,0 -3.5102,1.57157 -3.5102,3.5102c0,1.93863 1.57157,3.5102 3.5102,3.5102h117.59184c1.93863,0 3.5102,-1.57157 3.5102,-3.5102c0,-1.93863 -1.57157,-3.5102 -3.5102,-3.5102z"></path></g></g>
@@ -138,7 +143,6 @@ const canDeleteComments = (user, numberOfComments) => {
 				</li>
 			</ul>
 		</div>
-
 		`
 	} else {
 		canErase += "";
@@ -163,45 +167,45 @@ const clickToChangeComment = (numOfDiv, num) => {
 	specificDiv.innerHTML = "";
 
 	return specificDiv.innerHTML += `
-	<div class="reddit"> 	
-		<input type="text" id="inputReddit"/>
-		<ul class="cancelordoit" style="margin: 0; padding: 0;">	
-			<li class="options">	
-				<button type="button" class="btn btn-secondary mx-2" onclick="receiveNewComment(${numOfDiv + 4})" id="hola">
-					Aceptar
-				</button>
-			</li>
-			<li class="options">
-			<button class="btn btn-danger" onclick="showOldComments()">
-				Cancelar
-			</button>				
-			</li>
-		</ul>
-	<div>`
+		<div class="reddit"> 	
+			<input type="text" id="inputReddit"/>
+			<ul class="cancelordoit" style="margin: 0; padding: 0;">	
+				<li class="options">	
+					<button type="button" class="btn btn-secondary mx-2" onclick="receiveNewComment(${numOfDiv + 4})" id="hola">
+						Aceptar
+					</button>
+				</li>
+				<li class="options">
+				<button class="btn btn-danger" onclick="showOldComments()">
+					Cancelar
+				</button>				
+				</li>
+			</ul>
+		<div>
+	`
 
 }
 
 const editComment = (num) => {
 
-	let groupOfComments = document.querySelectorAll(".salchicha");
+	let groupOfComments = document.querySelectorAll(".editor-option");
 
 	for (let numOfDiv = 0; numOfDiv < groupOfComments.length; numOfDiv++) {
 		groupOfComments[numOfDiv].addEventListener('click', (event) => clickToChangeComment(numOfDiv))
 	}
 
 }
-const averageStars = () => {
+const averageStarsOfComments = () => {
 	let sumOfStars = 0;
 	staticComments[0].forEach(comment => {
-		sumOfStars += comment.score
+		sumOfStars += comment.score;
 	});
-	let average = Math.round(sumOfStars / staticComments[0].length);
 
-	let starsIcon = yellowStart(average) + blackStart(5 - average)
+	const average = Math.round(sumOfStars / staticComments[0].length);
 
-	let asd = ` ${starsIcon}` + " " + staticComments[0].length + " opiniones" +``;
+	const starsIcon = yellowStart(average) + blackStart(5 - average)
 
-	return asd;
+	return `${starsIcon} ${staticComments[0].length} opiniones`;
 }
 
 function showOldComments() {
@@ -212,27 +216,28 @@ function showOldComments() {
 
 	for (let product of staticComments[0]) {
 
-	let numberOfComments = document.querySelectorAll('.commentsTd');
+		let numberOfComments = document.querySelectorAll('.commentsTd');
 
-		commentsHTML.innerHTML +=
-		`<td class="commentsTd">
-			<div class="cuestion">
-				<p>Nombre de usuario: ${product.user}</p> 
-				<p>Puntuación: ${yellowStart(product.score)} ${blackStart(5 - product.score)}</p>
-				<div class="opinion">
-					<p class="es">Opinión: ${product.description}</p>  
+		commentsHTML.innerHTML += `
+			<td class="commentsTd">
+				<div class="cuestion">
+					<p>Nombre de usuario: ${product.user}</p> 
+					<p>Puntuación: ${yellowStart(product.score)} ${blackStart(5 - product.score)}</p>
+					<div class="opinion">
+						<p class="es">Opinión: ${product.description}</p>  
+					</div>
+					<p> Fecha: ${product.dateTime}</p> 
 				</div>
-				<p> Fecha: ${product.dateTime}</p> 
-			</div>
-			<div>	
-			 	${canDeleteComments(product.user, numberOfComments.length)}	
-		 	</div>
-		</td>`
+				<div>	
+				 	${canDeleteComments(product.user, numberOfComments.length)}	
+			 	</div>
+			</td>
+		`
 
-	}	
+	}
 	let num = document.querySelectorAll('.commentsTd')
 
-	editComment(num.length) 
+	editComment(num.length)
 
 }
 
@@ -253,7 +258,7 @@ const toComments = (e) => {
 		staticComments[0].push({
 			dateTime: date,
 			user: JSON.parse(valueName)[0].name,
-			score: Number(valueScore),
+			score: Number(valueScore < 6 ? valueScore : valueScore = 5),
 			description: valueDescription
 		})
 
@@ -268,7 +273,6 @@ const toComments = (e) => {
 btnEnviar.addEventListener('click', toComments);
 
 document.addEventListener("DOMContentLoaded", async function(e) {
-
 
 	/*COMMENTS*/
 	getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj) {
